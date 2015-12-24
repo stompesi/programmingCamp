65ba758961c3a672f980a8d528aa5a2b660b0cc3
stompesi <stompesi@gmail.com>
2015-12-24 15:19:21 +0900

    1. 반복문 n -> n / 2로 변경 2. 불필요한 변수 제거 : result

--- programmingCamp/c/method_5.c
+++ programmingCamp/c/method_5.c
@@ -1,31 +1,27 @@
 #include "method.h"
 
 int problem_5() {
-	int i, n, result = 1;
+	int input_number;
+	int i;
 
-	scanf("%d", &n);
+	scanf("%d", &input_number);
 
 	// 소수는 2부터 시작
 	// 나눈 나머지가 0이면, 나누어 떨어지는 것이니 소수가 아니다.
 	// n이 나누어 떨어질때 n이 i와 같다면 소수 아니면 소수가 아니므로 출력후 종료
 
-	if (n < 2) {
-		printf("잘못된 입력입니다.\n");
+	if(input_number < 2) {
+		printf("입력한 %d는 소수가 아닙니다.\n", input_number);	
 		return 0;
 	}
 
-	for (i = 2; i <= n; i++) {
-		if (n % i == 0) {
-			if (n == i) {
-				printf("입력한 %d는 소수입니다.\n", n);
-				break;
-			} else {
-				printf("입력한 %d는 소수가 아닙니다.\n", n);
-				break;
-			}
+	for(i = 2; i <= input_number / 2; i++) {
+		if (input_number % i == 0) {
+			printf("입력한 %d는 소수가 아닙니다.\n", input_number);	
+			return 0;
 		}
 	}
 	
-
+	printf("입력한 %d는 소수입니다.\n", input_number);
 	return 0;
 }
