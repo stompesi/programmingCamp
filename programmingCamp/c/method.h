c128809d714c4a0cf2cc3797967fd0cc2c5c423c
Whynn <fine612@gmail.com>
2015-12-24 10:32:16 +0900

    C언어 레벨1 문제 6번 풀었습니다. 난수를 발생시켜 몇번만에 맞추는지 출력하는 문제입니다. 실행시마다 다른 난수를 발생시키기위해 srand()로 시드값을 시간으로 설정했습니다.

--- programmingCamp/c/method.h
+++ programmingCamp/c/method.h
@@ -2,10 +2,13 @@
 #define METHOD_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int problem_1();
 int problem_2();
 int problem_3();
 int problem_4();
+int problem_6();
 
 #endif
