6a2960f751efb387330ec58ca404985e018bf3d7
Choi ji hun <choijh5414@naver.com>
2016-01-04 13:58:23 +0900

    입력 값이 16보다 큰 경우는 예외처리 하는 것으로 수정.

--- programmingCamp/c/method_10.c
+++ programmingCamp/c/method_10.c
@@ -12,6 +12,10 @@ int problem_10() {
 		printf("양의 정수가 아닙니다.\n");
 		return 0;
 	}
+	else if(input_number > 16) {
+		printf("표현 범위를 넘습니다. 17보다 작은 수를 입력하세요.");
+		return 0;
+	}
 
 	printf("%d! = %d\n", input_number, factorial(input_number));
 }
