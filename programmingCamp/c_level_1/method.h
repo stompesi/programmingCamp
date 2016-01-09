75f78177c89f5ead3d382820e99aaaf49f853df8
cjh5414 <cjh5414@gmail.com>
2016-01-09 18:18:40 +0900

    행렬의 각 행, 열의 합을 구하는 문제.

--- programmingCamp/c_level_1/method.h
+++ programmingCamp/c_level_1/method.h
@@ -2,25 +2,7 @@
 #define METHOD_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int problem_1();
-int problem_2();
-int problem_3();
-int problem_4();
-int problem_5();
-int problem_7();
-int problem_10();
-int problem_18();
-int problem_17();
-int problem_16();
-int problem_15();
-int problem_14();
-int problem_13();
-int problem_6();
-int problem_7();
-int problem_11();
-int problem_12();
+int problem_8();
 
 #endif
