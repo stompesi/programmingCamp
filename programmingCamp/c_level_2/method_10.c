f82a5dd89024e1d5aff1b77230240efd362ec373
jhKang1313 <jinhuk1313@gmail.com>
2016-01-10 17:55:04 +0900

    C 레벨 2 문제 10. 비주얼 2012

---
+++ programmingCamp/c_level_2/method_10.c
@@ -0,0 +1,26 @@
+#include"method.h"
+#include<stdio.h>
+
+#define CRITICAL_VALUE 0.000001
+int get_factorial(int input){
+	int result = 1, i;
+	for(i = 0 ; i < input ; i++){
+		result *= (i+1);
+	}
+	return result;
+}
+long double get_inverse(int value){
+	return 1./value;
+}
+void problem_10(){
+	int number = 1;
+	long double prev_result = 0, curr_result = 1;
+	while(1){
+		curr_result += get_inverse(get_factorial(number++));
+		if((curr_result - prev_result) < CRITICAL_VALUE){
+			break;
+		}
+		prev_result = curr_result;
+	}
+	printf("Euler Number >> %.10f\n", curr_result);
+}
