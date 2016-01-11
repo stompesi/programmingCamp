692fb48c1cfe0a946cad499744d814131b8681d2
stompesi <stompesi@gmail.com>
2016-01-11 13:11:38 +0900

    method 추가

--- programmingCamp/c_level_1/method.h
+++ programmingCamp/c_level_1/method.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 #include <time.h>
+#include <math.h>
 
 int problem_1();
 int problem_2();
@@ -13,6 +15,8 @@ int problem_5();
 int problem_6();
 int problem_7();
 int problem_8();
+//int problem_9();
+
 int problem_10();
 int problem_11();
 int problem_12();
@@ -22,5 +26,157 @@ int problem_15();
 int problem_16();
 int problem_17();
 int problem_18();
+//int problem_19();
 
+//int problem_20();
+//int problem_21();
+//int problem_22();
+//int problem_23();
+//int problem_24();
+//int problem_25();
+//int problem_26();
+//int problem_27();
+//int problem_28();
+//int problem_29();
+//int problem_30();
+//
+//int problem_31();
+//int problem_32();
+//int problem_33();
+//int problem_34();
+//int problem_35();
+//int problem_36();
+//int problem_37();
+//int problem_38();
+//int problem_39();
+//
+//int problem_40();
+//int problem_41();
+//int problem_42();
+//int problem_43();
+//int problem_44();
+//int problem_45();
+//int problem_46();
+//int problem_47();
+//int problem_48();
+//int problem_49();
+//
+//int problem_50();
+//int problem_51();
+//int problem_52();
+//int problem_53();
+//int problem_54();
+//int problem_55();
+//int problem_56();
+//int problem_57();
+//int problem_58();
+//int problem_59();
+//
+//int problem_60();
+//int problem_61();
+//int problem_62();
+//int problem_63();
+//int problem_64();
+//int problem_65();
+//int problem_66();
+//int problem_67();
+//int problem_68();
+//int problem_69();
+//int problem_70();
+//
+//int problem_70();
+//int problem_71();
+//int problem_72();
+//int problem_73();
+//int problem_74();
+//int problem_75();
+//int problem_76();
+//int problem_77();
+//int problem_78();
+//int problem_79();
+//int problem_80();
+//
+//int problem_80();
+//int problem_81();
+//int problem_82();
+//int problem_83();
+//int problem_84();
+//int problem_85();
+//int problem_86();
+//int problem_87();
+//int problem_88();
+//int problem_89();
+//int problem_90();
+//
+//int problem_90();
+//int problem_91();
+//int problem_92();
+//int problem_93();
+//int problem_94();
+//int problem_95();
+//int problem_96();
+//int problem_97();
+//int problem_98();
+//int problem_99();
+//int problem_100();
+//
+//int problem_100();
+//int problem_101();
+//int problem_102();
+//int problem_103();
+//int problem_104();
+//int problem_105();
+//int problem_106();
+//int problem_107();
+//int problem_108();
+//int problem_109();
+//int problem_110();
+//
+//int problem_110();
+//int problem_111();
+//int problem_112();
+//int problem_113();
+//int problem_114();
+//int problem_115();
+//int problem_116();
+//int problem_117();
+//int problem_118();
+//int problem_119();
+//int problem_120();
+//
+//int problem_120();
+//int problem_121();
+//int problem_122();
+//int problem_123();
+//int problem_124();
+//int problem_125();
+//int problem_126();
+//int problem_127();
+//int problem_128();
+//int problem_129();
+//int problem_130();
+//
+//int problem_130();
+//int problem_131();
+//int problem_132();
+//int problem_133();
+//int problem_134();
+//int problem_135();
+//int problem_136();
+//int problem_137();
+//int problem_138();
+//int problem_139();
+//int problem_140();
+//
+//int problem_140();
+//int problem_141();
+//int problem_142();
+//int problem_143();
+//int problem_144();
+//int problem_145();
+//int problem_146();
+//int problem_147();
+//int problem_148();
+//int problem_149();
+//int problem_150();
 #endif
