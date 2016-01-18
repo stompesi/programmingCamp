67215298d0bf1c2907683d1f53d7168af02a528c
jhKang1313 <jinhuk1313@gmail.com>
2016-01-19 02:32:08 +0900

    c++ 레벨 2 문제 1. 비주얼 2012

--- programmingCamp/c++_level_2/main.cpp
+++ programmingCamp/c++_level_2/main.cpp
@@ -1,5 +1,5 @@
 #include"method.h"
 int main(int argc, char* argv[]){
-	/*Effector effector;
-	effector.Problem1(argc, argv);*/
+	Effector effector;
+	effector.Problem1(argc, argv);
 }
--- programmingCamp/c++_level_2/method_1.cpp
+++ programmingCamp/c++_level_2/method_1.cpp
@@ -1,28 +1,56 @@
 #include"method.h"
 #include<iostream>
 #include<algorithm>
+#include<string>
 #include<vector>
 using namespace std;
 class Utility{
 	vector<string> aVectorStorage;
-	string* aWord;
+	string* apWord;
+	int aSize;
 public:
 	Utility(int argc, char* argv[]);
 	void UsedSTL();
 	void DisusedSTL();
+	~Utility();
 };
 Utility::Utility(int argc, char* argv[]){
-	aWord = new string[argc];
+	apWord = new string[argc];
+	aSize = argc - 1;
 	for(int i = 1 ; i < argc ; i++){
 		aVectorStorage.push_back(argv[i]);
-		aWord[i] = argv[i];
+		apWord[i] = argv[i];
 	}
 }
+void Utility::DisusedSTL(){
+	string temp_string;
+	cout<<"DISUSE STL"<<endl;
+	for(int i = 0 ; i < aSize - 1 ; i++){
+		for(int j = i + 1 ; j < aSize ; j++){
+			if(apWord[i].compare(apWord[j]) > 0){
+				temp_string = apWord[i];
+				apWord[i] = apWord[j];
+				apWord[j] = temp_string;
+			}
+		}
+	}
+	for(int i = 0 ; i < aSize ; i++){
+		cout<<apWord[i]<<endl;
+	}
+}
+Utility::~Utility(){
+	delete apWord;
+}
 void Utility::UsedSTL(){
-
-
+	cout<<"USE STL"<<endl;
+	sort(aVectorStorage.begin(), aVectorStorage.end());
+	for(int i = 0 ; i < aVectorStorage.size() ; i++){
+		cout<<aVectorStorage[i]<<endl;
+	}
 }
 void Effector::Problem1(int argc, char* argv[]){
 	Utility util(argc, argv);
+	util.UsedSTL();
+	util.DisusedSTL();
 	
 }
