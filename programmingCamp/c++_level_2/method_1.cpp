975371d9e0cb910568f8649af0e39eb2664c6528
jhKang1313 <jinhuk1313@gmail.com>
2016-01-19 02:33:43 +0900

    c++ 레벨 2 문제 1. 완성

--- programmingCamp/c++_level_2/method_1.cpp
+++ programmingCamp/c++_level_2/method_1.cpp
@@ -12,14 +12,43 @@ public:
 	Utility(int argc, char* argv[]);
 	void UsedSTL();
 	void DisusedSTL();
+	void ShowWordFrequency();
 	~Utility();
 };
+void Utility::ShowWordFrequency(){
+	int count = 1;
+	cout<<endl<<"Using Vector"<<endl;
+	for(int i = 0 ; i < aSize ; i++){
+		for(int j = i+1 ; j < aSize ; j++){
+			if(aVectorStorage[i] == aVectorStorage[j]){
+				count++;
+				i++;
+			}
+		}
+		cout<<aVectorStorage[i]<<">>"<<count<<endl;
+		count = 1;
+	}
+	count = 1;
+	cout<<endl<<"Using Array"<<endl;
+	for(int i = 0 ; i < aSize ; i++){
+		for(int j = i+1 ; j < aSize ; j++){
+			if(apWord[i] == apWord[j]){
+				count++;
+				i++;
+			}
+		}
+		cout<<apWord[i]<<">>"<<count<<endl;
+		count = 1;
+	}
+
+
+}
 Utility::Utility(int argc, char* argv[]){
 	apWord = new string[argc];
 	aSize = argc - 1;
 	for(int i = 1 ; i < argc ; i++){
 		aVectorStorage.push_back(argv[i]);
-		apWord[i] = argv[i];
+		apWord[i-1] = argv[i];
 	}
 }
 void Utility::DisusedSTL(){
@@ -39,7 +68,7 @@ void Utility::DisusedSTL(){
 	}
 }
 Utility::~Utility(){
-	delete apWord;
+	delete []apWord;
 }
 void Utility::UsedSTL(){
 	cout<<"USE STL"<<endl;
@@ -51,6 +80,8 @@ void Utility::UsedSTL(){
 void Effector::Problem1(int argc, char* argv[]){
 	Utility util(argc, argv);
 	util.UsedSTL();
+	cout<<endl;
 	util.DisusedSTL();
+	util.ShowWordFrequency();
 	
 }
