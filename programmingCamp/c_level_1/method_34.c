b7e97b36ff71a57dfe0e56f56b073c8fb9087641
stompesi <stompesi@gmail.com>
2016-01-24 10:14:21 +0900

    C / Lebel_1 / 문제_34

--- programmingCamp/c_level_1/method_34.c
+++ programmingCamp/c_level_1/method_34.c
@@ -1,5 +1,28 @@
 #include "method.h"
 
 int problem_34() {
+	char stmt[80];
+	char *ps, *pe;
 
+	printf("회문을 판별하는 프로그램입니다.\n");
+	printf("문자열을 입력하세요 : ");
+	gets(stmt);
+
+	ps = stmt;
+	pe = &stmt[strlen(stmt) - 1];
+
+	while(1) {
+		if(ps == pe) {
+			printf("회문입니다.\n");
+			break;
+		}
+		if(*ps == *pe) {
+			ps++;
+			pe--;
+		} else {
+			printf("회문이 아닙니다.\n");
+			break;
+		}
+	}
+	return 0;
 }
