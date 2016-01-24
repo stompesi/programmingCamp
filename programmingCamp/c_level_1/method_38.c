a91518cc262756059c2f034c425d8c67c0799689
stompesi <stompesi@gmail.com>
2016-01-24 10:13:28 +0900

    C / Lebel_1 / 문제_38

--- programmingCamp/c_level_1/method_38.c
+++ programmingCamp/c_level_1/method_38.c
@@ -1,5 +1,28 @@
 #include "method.h"
 
 int problem_38() {
+	int a, b, c;
+	int max = 0;
+	printf("제일 큰 수를 출력하는 프로그램입니다.\n");
 
+	printf("1번째 정수 : ");
+	scanf("%d", &a);
+	printf("2번째 정수 : ");
+	scanf("%d", &b);
+	printf("3번째 정수 : ");
+	scanf("%d", &c);
+
+	if(a > b) {
+		max = a;
+	} else {
+		max = b;
+	}
+	
+	if(max < c) {
+		max = c;
+	}
+
+	printf("제일 큰 수 : %d\n", max);
+
+	return 0;
 }
