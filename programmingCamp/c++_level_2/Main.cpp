9922736c3dd5fd803cb49659a602d15969dac44a
stompesi <stompesi@gmail.com>
2016-01-24 17:34:46 +0900

    C++/Level_2/문제 세팅 -  메소드 추가

--- programmingCamp/c++_level_2/Main.cpp
+++ programmingCamp/c++_level_2/Main.cpp
@@ -1,6 +1,87 @@
-#include"method.h"
-
+#include "method.h"
 int main() {
+	int select;
 	Effector effector;
-	effector.Problem5();
+
+	while(1) {
+		system("cls");
+		printf("원하는 문제 번호 선택 (1~143) - 종료(0) : >> ");
+		scanf("%d", &select);			
+		fflush(stdin);
+		switch(select) {
+		case 0:
+			return 0;
+		case 1:
+			effector.Problem1();
+			break;
+		case 2:
+			effector.Problem2();
+			break;
+		case 3:
+			effector.Problem3();
+			break;
+		case 4:
+			effector.Problem4();
+			break;
+		case 5:
+			effector.Problem5();
+			break;
+		case 6:
+			effector.Problem6();
+			break;
+		case 7:
+			effector.Problem7();
+			break;
+		case 8:
+			effector.Problem8();
+			break;
+		case 9:
+			effector.Problem9();
+			break;
+		case 10:
+			effector.Problem10();
+			break;
+		case 11:
+			effector.Problem11();
+			break;
+		case 12:
+			effector.Problem12();
+			break;
+		case 13:
+			effector.Problem13();
+			break;
+		case 14:
+			effector.Problem14();
+			break;
+		case 15:
+			effector.Problem15();
+			break;
+		case 16:
+			effector.Problem16();
+			break;
+		case 17:
+			effector.Problem17();
+			break;
+		case 18:
+			effector.Problem18();
+			break;
+		case 19:
+			effector.Problem19();
+			break;
+		case 20:
+			effector.Problem20();
+			break;
+		case 21:
+			effector.Problem21();
+			break;
+		case 22:
+			effector.Problem22();
+			break;
+		default:
+			printf("존재하지않는 문제번호.\n");
+		}
+		printf("계속하시려면 아무키나 입력해주세요.");
+		getch();
+		fflush(stdin);
+	}
 }
--- programmingCamp/c++_level_2/method.h
+++ programmingCamp/c++_level_2/method.h
@@ -5,13 +5,37 @@
 #include<iostream>
 #include<string>
 #include<conio.h>
+#include<vector>
+#include<algorithm>
 #include<Windows.h>
 
 using namespace std;
 
-class Effector {
+class Effector{
 public:
+	void Problem1(){};
+	void Problem2(){};
+	void Problem3(){};
+	void Problem4(){};
 	void Problem5();
+	void Problem6(){};
+	void Problem7(){};
+	void Problem8(){};
+	void Problem9();
+	void Problem10(){};
 
+	void Problem11(){};
+	void Problem12(){};
+	void Problem13(){};
+	void Problem14(){};
+	void Problem15(){};
+	void Problem16(){};
+	void Problem17(){};
+	void Problem18();
+	void Problem19(){};
+	void Problem20(){};
+
+	void Problem21(){};
+	void Problem22(){};
 };
 #endif
