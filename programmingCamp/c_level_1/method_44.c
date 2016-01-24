ed6fbf4f25ad69b48a63620dd069ccf8c893d2da
stompesi <stompesi@gmail.com>
2016-01-24 10:10:37 +0900

    C / Lebel_1 / 문제_44

--- programmingCamp/c_level_1/method_44.c
+++ programmingCamp/c_level_1/method_44.c
@@ -1,5 +1,18 @@
 #include "method.h"
 
 int problem_44() {
+	int n;
 
+	printf("10000 미만의 숫자를 입력하면 단위 별로 구분하여 숫자를 출력\n");
+
+	printf("정수 n 입력 : ");
+	scanf("%d", &n);
+	
+	printf("단위\n");
+	printf("1000단위 : %d\n", n / 1000);
+	printf(" 100단위 : %d\n", (n - ((n / 1000) * 1000)) / 100);
+	printf("  10단위 : %d\n", (n - ((n / 100) * 100)) / 10);
+	printf("   1단위 : %d\n", n % 10);
+
+	return 0;
 }
