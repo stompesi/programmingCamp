3e16393e183c8197b11744a3bb4784e9a01973a0
stompesi <stompesi@gmail.com>
2016-01-24 10:11:30 +0900

    C / Lebel_1 / 문제_42

--- programmingCamp/c_level_1/method_42.c
+++ programmingCamp/c_level_1/method_42.c
@@ -1,5 +1,29 @@
 #include "method.h"
 
 int problem_42() {
+	int i, j;
 
+	for( i = 0 ; i <= 5 ; i++) {
+		for(j = 0 ; j <= i ; j++) {
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	for(i =  5; i >= 0 ; i--) {
+		for(j = 0 ; j <= i ; j++) {
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	for(i =5 ; i >= 0 ; i--) {
+		for(j = i ; j >=0 ; j--) {
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+	return 0;
 }
