840f750b60856babd5f9a287b8ccca7768d1e8bb
stompesi <stompesi@gmail.com>
2016-01-24 10:11:12 +0900

    C / Lebel_1 / 문제_43

--- programmingCamp/c_level_1/method_43.c
+++ programmingCamp/c_level_1/method_43.c
@@ -1,5 +1,22 @@
 #include "method.h"
 
+void reverse_num_43(int n) {
+	if(n > 10) {
+		printf("%d", n % 10);
+		reverse_num_43(n / 10);
+	} else {
+		printf("%d", n);
+	}
+}
 int problem_43() {
+	int n;
 
+	printf("정수를 거꾸로 출력하는 프로그램\n");
+
+	printf("숫자를 입력: ");
+	scanf("%d", &n);
+	printf("결과 : ");
+	reverse_num_43(n);
+	printf("\n");
+	return 0;
 }
