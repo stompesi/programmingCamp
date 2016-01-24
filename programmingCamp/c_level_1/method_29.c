6edca1bc5a153de86da3e194a4e35d8d811112bf
stompesi <stompesi@gmail.com>
2016-01-24 10:14:30 +0900

    C / Lebel_1 / 문제_29

--- programmingCamp/c_level_1/method_29.c
+++ programmingCamp/c_level_1/method_29.c
@@ -1,5 +1,23 @@
 #include "method.h"
+#define G 9.80665
 
 int problem_29() {
+	int t=0;
+	double h = 200;
 
+	printf("시간\t높이\n");
+	printf("===================\n");
+	while(1) {
+		if(t == 0) {
+			printf("%2d초\t%.0lfM\n", t, h);
+		} else {
+			h = 200 - 0.5 * G * t * t;
+			if(h < 0) {
+				break;
+			}
+			printf("%2d초\t%.1lfM\n", t, h);
+		}
+		t++;
+	}
+	return 0;
 }
