a8d3ef0375861505aef5f3c2a2aee0f7d4c5cd98
cjh5414 <cjh5414@gmail.com>
2016-01-27 17:29:24 +0900

    5개 이외에도 가능하도록 수정.

--- programmingCamp/c++_level_2/method_6.cpp
+++ programmingCamp/c++_level_2/method_6.cpp
@@ -1,29 +1,23 @@
 #include"method.h"
+#include"sstream"
 
 void Effector::Problem6() {
-	string str, max_str = " ";
-	int i;
+	string str, max_str = "";
+	int i = 0;
 
 	cout << "임의의 이름을 ; 으로 구분하여 입력하시오(단, 마지막 ; 는 생략이 가능합니다)." << endl;
 
 	cin.ignore(1, '\n');
 
-	for (i = 0; i < 4; i++) {
-		getline(cin, str, ';');
-		cout << i + 1 << " : " << str << endl;
+	getline(cin, str);
+	stringstream s_stream(str);
+	while (getline(s_stream, str, ';')) {
+		i++;
+		cout << i << " : " << str << endl;
 		if (str.size() > max_str.size()) {
 			max_str = str;
 		}
 	}
-
-	getline(cin, str);
-	if (str.at(str.size() - 1) == ';') {
-		str.erase(str.size() - 1, 1);
-	}
-	if (str.size() > max_str.size()) {
-		max_str = str;
-	}
-	cout << i + 1 << " : " << str << endl;
 
 	cout << "가장 긴 이름은 " << max_str << endl;
 }
